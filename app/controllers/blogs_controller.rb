5619a9877b38ea6476cad9257fac2980e0e6c7f7
chinamiii <chinami.2929@icloud.com>
2021-08-05 14:58:23 +0000

    [finish]3-8

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,28 +1,50 @@
 class BlogsController < ApplicationController
+
   def index
-    @blog = Blog.
+    @blogs = Blog.all
+    p "標準出力にのみ反映"
+    logger.debug("標準出力とログファイルに記録される")
   end
 
   def show
+    @blog = Blog.find(params[:id])
+
   end
 
   def new
     @blog = Blog.new
   end
-  
+
   def create
-    blog = Blog.new(blog_params)
-    blog.save
-    redirect_to blogs_path
+    @blog = Blog.new(blog_params)
+    binding.pry
+   if @blog.save
+     redirect_to blog_path(@blog.id)
+   else
+     render :new
+   end
+
   end
 
   def edit
+    @blog = Blog.find(params[:id])
   end
-end
 
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
+  end
 
-private
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
+  end
 
-def blog_params
-  params.require(:blog).permit(:title, :category, :body)
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :category, :body)
+  end
+
 end
